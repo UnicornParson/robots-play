7f236279cfa91451e9abda0f3889e4b3d78c8798
Dieter <phodiale@ya.ru>
2024-06-10 13:04:52 +0300

    add some functions

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
 import mmap
 import tqdm
-
+import statistics
 def get_num_lines(file_path) -> int:
 	fp = open(file_path, "r+")
 	buf = mmap.mmap(fp.fileno(), 0)
@@ -30,11 +30,22 @@ def get_diff(l: list, r: list):
 		d.append(r[i] - l[i])
 	return d
 
+
+def nb_diff(l) -> list:
+	if not l or len(l) < 2:
+		return []
+	diff = []
+	prev = l[0]
+	for v in l[1:]:
+		diff.append(v - prev)
+		prev = v
+	return diff
+
 def normalize(l:list)->list:
 	if not l:
 		return []
 	ret = []
-	nn = notNull(l)
+	nn = not_null(l)
 	if not nn:
 		return []
 	base = min(nn)
@@ -49,6 +60,11 @@ def chunks(L, n):
 	for i in range(0, len(L), n):
 		yield L[i:i+n]
 
+def stats(l) -> str:
+	_min = min(l)
+	_max = max(l)
+	_mid = statistics.median(l)
+	return "{} - {} - {} len:{} range:{}".format(_min, _mid, _max, len(l), (_max - _min))
 
 def load_markers(fname: str, marker: str, extractor) -> list:
 	result = []
@@ -59,10 +75,10 @@ def load_markers(fname: str, marker: str, extractor) -> list:
 			if marker in line:
 				pos = line.find(marker) + len(marker)
 				tail = line[pos:].strip()
-				#parts = tail.split(":")
 				try:
 					m = extractor(tail)
 					result.append(m)
 				except Exception as e:
 					print("invalid line ( ", line, ")", str(e))
 					continue
+	return result
